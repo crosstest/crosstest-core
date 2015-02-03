273b334491dd359e6d24be5135f535d481ddad90
Max Lincoln <max@devopsy.com>
2015-02-02 19:39:45 -0500

    Method for stdout logger

--- lib/crosstest/core/logger.rb
+++ lib/crosstest/core/logger.rb
@@ -8,9 +8,30 @@ module Crosstest
       end
 
       def new_logger(io = $stdout, level = :debug)
-        StdoutLogger.new(io).tap do | logger |
+        stdout_logger(io).tap do | logger |
           logger.level = Util.to_logger_level(level)
         end
+      end
+
+      # Construct a new standard out logger.
+      #
+      # @param stdout [IO] the IO object that represents stdout (or similar)
+      # @param color [Symbol] color to use when outputing messages
+      # @return [StdoutLogger] a new logger
+      # @api private
+      def stdout_logger(stdout, color = nil)
+        logger = Crosstest::Core::StdoutLogger.new(stdout)
+        # if Crosstest.tty?
+        if stdout.tty? && color
+          logger.formatter = proc do |_severity, _datetime, _progname, msg|
+            Core::Color.colorize("#{msg}", color).concat("\n")
+          end
+        else
+          logger.formatter = proc do |_severity, _datetime, _progname, msg|
+            msg.concat("\n")
+          end
+        end
+        logger
       end
 
       def log_level=(level)
